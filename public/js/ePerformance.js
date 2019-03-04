331b97aff29954e0e1614b1179fbc810fc63d608
Maxwell Chan <ccsmaxwell@link.cuhk.edu.hk>
2019-03-04 15:06:08 +0800

    draw graph for perfoemance

--- public/js/ePerformance.js
+++ public/js/ePerformance.js
@@ -9,12 +9,38 @@ $.ajax({
 
 		let arrSort_voterTime = res.concat().sort((a,b) => new Date(a.voterTimestamp) - new Date(b.voterTimestamp));
 		let data_voterTime = [], count_voterTime = 0;
-		arrSort_voterTime.forEach(function(b){
-			data_voterTime.push({
-				x: new Date(b.voterTimestamp),
-				y: ++count_voterTime
-			})
-		})
+		let arrSort_recTime = res.concat().sort((a,b) => new Date(a.receiveTime) - new Date(b.receiveTime));
+		let data_recTime = [], count_recTime = 0;
+		let arrSort_blockTime = res.concat().sort((a,b) => new Date(a.blockCreatedAt) - new Date(b.blockCreatedAt));
+		let data_blockTime = [], count_blockTime = 0;
+		for(let bi in res){
+			if(bi>0 && arrSort_voterTime[bi-1].voterTimestamp==arrSort_voterTime[bi].voterTimestamp){
+				data_voterTime[data_voterTime.length-1].y = ++count_voterTime;
+			}else{
+				data_voterTime.push({
+					x: new Date(arrSort_voterTime[bi].voterTimestamp),
+					y: ++count_voterTime
+				})
+			}
+
+			if(bi>0 && arrSort_recTime[bi-1].receiveTime==arrSort_recTime[bi].receiveTime){
+				data_recTime[data_recTime.length-1].y = ++count_recTime;
+			}else{
+				data_recTime.push({
+					x: new Date(arrSort_recTime[bi].receiveTime),
+					y: ++count_recTime
+				})
+			}
+
+			if(bi>0 && arrSort_blockTime[bi-1].blockCreatedAt==arrSort_blockTime[bi].blockCreatedAt){
+				data_blockTime[data_blockTime.length-1].y = ++count_blockTime;
+			}else{
+				data_blockTime.push({
+					x: new Date(arrSort_blockTime[bi].blockCreatedAt),
+					y: ++count_blockTime
+				})
+			}
+		}
 
 		let ctx = $("#ballot_chart");
 		let lineChart = new Chart(ctx, {
@@ -24,7 +50,20 @@ $.ajax({
 					data: data_voterTime,
 					label: "Vote submit",
 					fill: false,
-					borderColor: "Blue"
+					borderColor: "Red",
+					steppedLine: true
+				},{
+					data: data_recTime,
+					label: "Vote receive",
+					fill: false,
+					borderColor: "Yellow",
+					steppedLine: true
+				},{
+					data: data_blockTime,
+					label: "Block generate",
+					fill: false,
+					borderColor: "Green",
+					steppedLine: true
 				}]
 			},
 			options: {
@@ -38,7 +77,14 @@ $.ajax({
 						},
 						distribution: 'linear'
 					}]
-				}
+				},
+				animation: {
+					duration: 0,
+				},
+				hover: {
+					animationDuration: 0,
+				},
+				responsiveAnimationDuration: 0,
 			}
 		});
 	}
